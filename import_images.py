a986e82e97f8bc0c60e4d39aa933aeae0214688e
aogarlid <aogarlid@gmail.com>
2019-01-22 10:57:09 -0800

    update import_images.py
    Signed-off-by: aogarlid <aogarlid@gmail.com>

--- import_images.py
+++ import_images.py
@@ -13,8 +13,8 @@ col_dir = 'NeedleImages/*.jpg'
 # creating a collection with the available images
 col = imread_collection(col_dir)
 
-img = col[1]
-img_gray = rgb2gray(img)
+im = col[1]
+img = rgb2gray(im)
 
 
 
