783ae9fe31c44bf8c3b37d1403fd1cd6f31da71a
aogarlid <aogarlid@gmail.com>
2019-02-06 12:04:12 -0800

    update image import and NeedleImages
    Signed-off-by: aogarlid <aogarlid@gmail.com>

--- import_images.py
+++ import_images.py
@@ -1,16 +1,47 @@
 '''
 import_images.py
-    convert images to gray-scale from rgb
+    determine whether images are greyscale
+    convert to greyscale if RGB
     file i/o to load images, matrix manipulation, standardization
 '''
 
-from skimage.color import rgb2gray
+import PIL
+import os
 from skimage.io import imread_collection
 
-# set NeedleImages collection path
-col_dir = 'NeedleImages/*.jpg'
+dir_name = 'NeedleImages/'
+imgs = []
+greyscale = []
+
 # creating a collection with the available images
-col = imread_collection(col_dir)
+col = imread_collection(os.path.join(dir_name, '*.jpg'))
 # select one image for analysis
-im = col[4]
-img = rgb2gray(im)
+im = col[14]
+
+print(type(im))
+print(im.shape)
+
+# create list of .jpg files in img directory
+for root, dirs, files in os.walk(dir_name):
+    for file in files:
+        if file.endswith('.jpg'):
+            imgs.append(file)
+
+# create function to test whether images are greyscale
+def is_grey_scale(img_path):
+    im = PIL.Image.open(img_path).convert('RGB')
+    w,h = im.size
+    for i in range(w):
+        for j in range(h):
+            r,g,b = im.getpixel((i,j))
+            if r != g != b:
+                return False
+    return True
+
+# test set of images for greyscale
+# for i in imgs:
+#     img = os.path.join(dir_name, i)
+#     greyscale.append(is_grey_scale(img))
+
+# print(greyscale)
+# print(len(greyscale))
